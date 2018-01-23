ea7d2421e70704ce11dcec4ad31ad3057f870911
Yukun Guo <gyk.net@gmail.com>
2018-01-23 10:32:05 +0800

    futures-bounded-mpsc: implement custom clone to fix the issue

--- futures-bounded-mpsc/src/main.rs
+++ futures-bounded-mpsc/src/main.rs
@@ -4,6 +4,8 @@ extern crate tokio_timer;
 
 mod inspect;
 
+use std::env;
+use std::mem;
 use std::time::Duration;
 
 use futures::stream;
@@ -13,7 +15,6 @@ use tokio_core::reactor::Core;
 use tokio_timer::Timer;
 
 fn inpect_channel<T>(tx: &Sender<T>) {
-    use std::mem;
     use std::sync::atomic::Ordering;
 
     let naked_sender = unsafe {
@@ -28,7 +29,29 @@ fn inpect_channel<T>(tx: &Sender<T>) {
         inspect::decode_state(state), maybe_parked);
 }
 
+fn custom_clone<T>(tx: &Sender<T>) -> Sender<T> {
+    let naked_tx = unsafe {
+        mem::transmute::<&Sender<T>, &inspect::Sender<T>>(&tx)
+    };
+    let sender_task = naked_tx.sender_task.clone();
+
+    let mut tx2 = tx.clone();
+    let naked_tx2 = unsafe {
+        mem::transmute::<&mut Sender<T>, &mut inspect::Sender<T>>(&mut tx2)
+    };
+    naked_tx2.sender_task = sender_task;
+    // Always sets `maybe_parked` to true so `sender_task` is locked every time. It degrades the
+    // performance but is logically correct.
+    naked_tx2.maybe_parked = true;
+    tx2
+}
+
 fn main() {
+    let use_custom_clone = match env::var("CUSTOM_CLONE") {
+        Ok(_) => true,
+        Err(_) => false,
+    };
+
     let mut core = Core::new().unwrap();
     let handle = core.handle();
 
@@ -46,14 +69,23 @@ fn main() {
         .for_each(move |x| {
             let x_clone = x;
 
-            let tx2 = tx.clone();
-            print!("[CLONE] ");
+            let tx2;
+            match use_custom_clone {
+                true => {
+                    tx2 = custom_clone(&tx);
+                    print!("[!CLONED] ");
+                }
+                false => {
+                    tx2 = tx.clone();
+                    print!("[CLONED]  ");
+                }
+            }
             inpect_channel(&tx2);
 
             tx2
                 .send(x)
                 .map(move |tx| {
-                    print!("[SENT]  ");
+                    print!("[SENT]    ");
                     inpect_channel(&tx);
                     println!("tx -> {}", x_clone);
                     ()
