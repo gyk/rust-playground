25bd8a225fac6bf1e4b8572fcd998e4c86f2afda
Yukun Guo <gyk.net@gmail.com>
2021-12-28 14:35:53 +0800

    + try-clipboard-win

---
+++ try-clipboard-win/src/main.rs
@@ -0,0 +1,40 @@
+use clipboard_win::{Clipboard, Getter};
+use clipboard_win::formats::*;
+
+fn main() {
+    let _clipboard = Clipboard::new().unwrap();
+
+    let mut image_buffer = Vec::new();
+    match RawData(CF_DIB).read_clipboard(&mut image_buffer) {
+        Ok(len) => {
+            println!("Read dib, len = {}", len);
+        }
+        Err(e) => println!("Read dib error {:?}", e),
+    }
+
+    let mut image_buffer = Vec::new();
+    match Bitmap.read_clipboard(&mut image_buffer) {
+        Ok(len) => {
+            println!("Read bitmap, len = {}", len);
+            return;
+        }
+        Err(e) => println!("Read bitmap error {:?}", e),
+    }
+
+    let mut file_list = Vec::new();
+    match FileList.read_clipboard(&mut file_list) {
+        Ok(len) => {
+            println!("Read file list, len = {}, list = {:#?}", len, file_list);
+            return;
+        }
+        Err(e) => println!("Read file list error {:?}", e),
+    }
+
+    let mut s = String::new();
+    if let Ok(len) = Unicode.read_clipboard(&mut s) {
+        println!("Read string, len = {}, content = '{}'", len, s);
+        return;
+    }
+
+    println!("Nothing in clipboard");
+}
